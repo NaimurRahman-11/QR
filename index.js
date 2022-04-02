e3c0eb27e021864c031e83fe2d1e19f30cb8bdaa
Naimur Rahman <nirzon.naim@gmail.com>
2022-04-03 00:30:07 +0600

    Almost Done!

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const wrapper = document.querySelector(".wrapper");
-generateBtn = wrapper.querySelector(".form button");
+const wrapper = document.querySelector(".wrapper"),
+generateBtn = wrapper.querySelector(".form button"),
+qrInput = wrapper.querySelector(".form input"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}';
+    qrImg.addEventListener("load", () => {
+        generateBtn.innerText = "Generate QR Code";
+        wrapper.classList.add("active");
+    });
+    
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
+});
